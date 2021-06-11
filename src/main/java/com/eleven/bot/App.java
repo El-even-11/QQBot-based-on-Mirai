99ac075a6ec91aa3b24716cf548eeae4a2b1a5c8
El-even-11 <zhao_ziqian@sjtu.edu.cn>
2021-06-11 08:32:27 +0800

    test for jdbc

--- src/main/java/com/eleven/bot/App.java
+++ src/main/java/com/eleven/bot/App.java
@@ -4,6 +4,7 @@ public class App {
 
     public static void main(String[] args) {
 
+        //Database.test();
         Bot Lass = new Bot();
         Lass.run();
 
--- src/main/java/com/eleven/bot/PostMessage.java
+++ src/main/java/com/eleven/bot/PostMessage.java
@@ -8,6 +8,7 @@ import static com.eleven.bot.Http.doPost;
 
 public class PostMessage {
     public static void postMessage(List<JSONObject> messageChains, String type, Long target, String sessionKey) {
+
         if (messageChains != null && messageChains.size() > 0) {
             for (JSONObject response : messageChains) {
                 response.put("sessionKey", sessionKey);
@@ -16,12 +17,10 @@ public class PostMessage {
                 String postResponse = doPost("http://0.0.0.0:8080/send" + type + "Message", response.toJSONString());
                 JSONObject jsonResponse = JSONObject.parseObject(postResponse);
 
-                if (jsonResponse != null) {
-                    if (jsonResponse.getIntValue("code") == 0) {
-                        System.out.println("post " + type + " message successfully");
-                    } else {
-                        System.out.println("post " + type + " fail");
-                    }
+                if (jsonResponse != null && jsonResponse.getIntValue("code") == 0) {
+                    System.out.println("post " + type + " message successfully");
+                } else {
+                    System.out.println("post " + type + " fail");
                 }
             }
         }
